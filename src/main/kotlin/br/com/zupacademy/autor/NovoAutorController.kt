48957b71e3d9713eb98dbb9b00d3b4b13e61fe96
Rodrigo Rocha <rodrigorochasantos@outlook.com>
2021-05-11 10:33:15 -0300

    Refatora classe NovoAutorController
    - Add location na resposta do método cadastra

--- src/main/kotlin/br/com/zupacademy/autor/NovoAutorController.kt
+++ src/main/kotlin/br/com/zupacademy/autor/NovoAutorController.kt
@@ -1,8 +1,10 @@
 package br.com.zupacademy.autor
 
+import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
 import io.micronaut.http.annotation.Controller
 import io.micronaut.http.annotation.Post
+import io.micronaut.http.uri.UriBuilder
 import io.micronaut.validation.Validated
 import javax.transaction.Transactional
 import javax.validation.Valid
@@ -13,9 +15,13 @@ class NovoAutorController(val autorRepository: AutorRepository) {
 
     @Post // equivalente ao @PostMapping do Spring
     @Transactional // essa anotação não é obrigatória, pois o método save já abre uma transação com o banco
-    fun cadastra(@Valid @Body autorRequest: NovoAutorRequest) {
+    fun cadastra(@Valid @Body autorRequest: NovoAutorRequest): HttpResponse<Any> {
         // o @Body é equivalente ao @RequestBody do Spring, porém aqui no Micronaut não é obrigatório.
         val novoAutor = autorRequest.toModel()
+
         autorRepository.save(novoAutor)
+
+        val location = UriBuilder.of("/autores/{id}").expand(mutableMapOf("id" to novoAutor.id))
+        return HttpResponse.created(location)
     }
 }
